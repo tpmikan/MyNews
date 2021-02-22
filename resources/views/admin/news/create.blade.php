acdfe60ad5ed6dbe1c15704d0452d49147850c95
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-02-22 02:49:36 +0000

    PHP/Laravel_11 task_commit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -6,18 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <title>MyNews</title>
+    
   </head>
   <body>
     
     @extends('layouts.admin')
     
-    @section('title', 'My プロフィール')
+    @section('title', 'ニュースの新規作成')
     
     @section('content')
       <div class="container">
         <div class="row">
           <div class="col-md-8 mx-auto">
-            <h2>My プロフィール</h2>
+            <h2>ニュース新規作成</h2>
           </div>
         </div>
       </div>
