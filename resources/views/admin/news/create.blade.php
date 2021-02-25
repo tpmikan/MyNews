32c2b96a806f3383a1dfa6d5e46996486a80e718
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-02-25 23:51:17 +0000

    PHP/Laravel_11 task_commit clean

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
+@section('title', 'ニュースの新規作成')
     
-    <title>MyNews</title>
-    
-  </head>
-  <body>
-    @extends('layouts.admin')
-    @section('title', 'ニュースの新規作成')
-    
-    @section('content')
+@section('content')
       <div class="container">
         <div class="row">
           <div class="col-md-8 mx-auto">
@@ -50,6 +39,4 @@
           </div>
         </div>
       </div>
-    @endsection
-  </body>
-</html>
+@endsection
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
     
-    <title>MyNews</title>
-  </head>
-  <body>
+@section('title', 'My プロフィール')
     
-    @extends('layouts.admin')
-    
-    @section('title', 'My プロフィール')
-    
-    @section('content')
+@section('content')
       <div class="container">
         <div class="row">
           <div class="col-md-8 mx-auto">
@@ -59,6 +48,4 @@
           </div>
         </div>
       </div>
-    @endsection
-  </body>
-</html>
+@endsection
