07ac1389b57ffcd8599118d46c9edac6506320a8
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-02-26 07:40:13 +0000

    PHP/Laravel_14 task_commit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -7,7 +7,7 @@
           <div class="col-md-8 mx-auto">
             <h2>ニュース新規作成</h2>
             <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
-            @if (count($errors) > 0)
+              @if (count($errors) > 0)
                 <ul>
                   @foreach ($errors->all() as $e)
                     <li>{{ $e }}</li>
@@ -30,7 +30,7 @@
                 <label class="col-md-2">画像</label>
                 <div class="col-md-10">
                   <input type="file" class="form-control-file" name="image">
-                </div>
+        　       </div>
               </div>
               {{ csrf_field() }}
               <input type="submit" class="btn btn-primary" value="更新">
