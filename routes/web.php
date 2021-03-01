8d6d4c9f57b55ca6733739db273c9590a2828718
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-03-01 23:21:42 +0000

    PHP/Laravel_16 commit

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,11 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
   
   Route::get('news', 'Admin\NewsController@index');
   
+  Route::get('news/edit', 'Admin\NewsController@edit');
+  Route::post('news/edit', 'Admin\NewsController@update');
+  
+  Route::get('news/delete', 'Admin\NewsController@delete');
+  
   Route::get('profile/create', 'Admin\ProfileController@add');
   Route::post('profile/create', 'Admin\ProfileController@create');
   
