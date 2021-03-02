81ab52ec9d99a8e1962481c67e1e19813c7f4dd9
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-03-02 06:52:36 +0000

    commit update

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -17,7 +17,7 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com"> 
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     
-     Styles 
+    <!-- Styles -->
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
     
