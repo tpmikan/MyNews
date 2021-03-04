da72036ead166a028fc25575c55023c3f4f410ea
Takahiro <blackwolf.tp.taka@outlook.jp>
2021-03-04 04:29:29 +0000

    PHP/Laravel_17 commit

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\News;
+use App\History;
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -87,7 +89,12 @@ class NewsController extends Controller
     // 該当するデータを上書きして保存する
     $news->fill($news_form)->save();
     
-    return redirect('admin/news');
+    $history = new History;
+    $history->news_id = $news->id;
+    $history->edited_at = Carbon::now();
+    $history->save();
+     
+    return redirect('admin/news/');
   }
   
   public function delete (Request $request)
